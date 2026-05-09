7bac4ba14139c7c4032647d19bcb7836b7aa30fd
ranvirsharmagit <ranvir.sharma088@gmail.com>
2026-05-09 22:19:52 +0530

    first script

---
+++ FirstPlaywright.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright
+
+def google_test():
+
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        print(page.title())
+        browser.close()
