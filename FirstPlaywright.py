eb664740a3d123667d75440e24542e6afd08cb8f
ranvirsharmagit <ranvir.sharma088@gmail.com>
2026-05-09 22:38:08 +0530

    sd

--- FirstPlaywright.py
+++ FirstPlaywright.py
@@ -2,7 +2,6 @@ from playwright.sync_api import sync_playwright
 
 def google_test():
 
-
     with sync_playwright() as p:
         browser = p.firefox.launch(headless=False)
         page = browser.new_page()
