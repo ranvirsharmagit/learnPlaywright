bae79d6d78ce540935ba1f8c67c7b1360aad9e47
ranvirsharmagit <ranvir.sharma088@gmail.com>
2026-05-09 23:34:44 +0530

    secodn file

--- Test_secondscript.py
+++ Test_secondscript.py
@@ -1,11 +1,9 @@
 from playwright.sync_api import sync_playwright
 
-def google_test():
-
+def test_google():
     with sync_playwright() as p:
-        browser = p.firefox.launch(headless=False)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://www.google.com")
-        assert "google" in page.title()
         print(page.title())
         browser.close()
---
+++ test_FirstPlaywright.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+def google_test():
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        assert "google" in page.title()
+        print(page.title())
+        browser.close()
